25c11ae1296ff51450eeaad782e179150ee59acd
Omkar-SK <omkar.katare06@gmail.com>
2025-10-20 15:26:17 +0530

    flash-connect

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
 const ExpressError = require("./utils/ExpressError.js");
-
+const session = require("express-session");
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
+const flash = require("connect-flash");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -31,9 +30,31 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
 app.get("/", (req,res) => {
     res.send("Hi, I am root");
 });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req,res,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
+
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/new", (req,res) => {
 router.get("/:id", wrapAsync(async(req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error", "Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -39,6 +43,7 @@ router.post("/", validateListing, wrapAsync(async (req,res,next) => {
     
     const newListing =  new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
     })
 );
@@ -47,6 +52,10 @@ router.post("/", validateListing, wrapAsync(async (req,res,next) => {
 router.get("/:id/edit", wrapAsync( async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
+        if(!listing){
+        req.flash("error", "Listing you requested for does not exist");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", {listing});
 }));
 
@@ -54,6 +63,7 @@ router.get("/:id/edit", wrapAsync( async (req,res) => {
 router.put("/:id",validateListing, wrapAsync( async (req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -61,6 +71,7 @@ router.put("/:id",validateListing, wrapAsync( async (req,res) => {
 router.delete("/:id",wrapAsync( async (req,res) => {
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings")
 }));
 
