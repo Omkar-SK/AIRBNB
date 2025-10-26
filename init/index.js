76b69be2c9913e31375c0ea37fd62286cf4ecf2d
Omkar-SK <omkar.katare06@gmail.com>
2025-10-26 15:34:13 +0530

    completed authorization

--- init/index.js
+++ init/index.js
@@ -17,6 +17,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: '68f8d2f7b886aa7a15dc2b08'}))
     await Listing.insertMany(initData.data);
     console.log("data was inititalised");
 
